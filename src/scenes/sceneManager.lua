122995dd66b9cd4c569ca1afe2c88845b7ced86f
Oli <OliverWDGarrett@gmail.com>
2025-08-15 16:43:18 +1200

    Made scenes auto-load

--- src/scenes/sceneManager.lua
+++ src/scenes/sceneManager.lua
@@ -11,15 +11,29 @@ local nameToScene = {--[[
     [name] -> Scene
 ]]}
 
-for _, name in ipairs({
-    "forest" })
-do
-    nameToScene[name] = require("src.scenes." .. name .. "." .. name)
+
+
+local allScenes = objects.Array()
+
+local SCENE_PATH = "src/scenes/"
+for _, folder in ipairs(love.filesystem.getDirectoryItems(SCENE_PATH)) do
+    if love.filesystem.getInfo(SCENE_PATH .. folder, "directory") then
+        allScenes:add(folder)
+    end
+end
+
+for _, name in ipairs(allScenes) do
+    local scene = require("src.scenes." .. name .. "." .. name)
+    if scene.init then
+        scene:init()
+    end
+    nameToScene[name] = scene
 end
 
 
 
 function sceneManager.gotoScene(sceneName)
+    assert(nameToScene[sceneName])
     currentScene = nameToScene[sceneName]
 end
 
