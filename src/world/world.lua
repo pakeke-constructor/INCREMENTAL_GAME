3064cca640202375108d0268920fea211bceef14
Oli <OliverWDGarrett@gmail.com>
2025-09-04 12:23:19 +1200

    work on token/world stuff

--- src/world/world.lua
+++ src/world/world.lua
@@ -9,51 +9,121 @@ Used by the harvest-scene
 ]]
 
 
+---@class world
+---@field partition objects.Partition
 local world = {}
 
 
 function world.init()
-    world.tokens = objects.Set()
+    world.tokens = objects.BufferedSet()
+    world.entities = objects.BufferedSet()
 
-    world.thingAddBuffer = objects.Set()
-    world.things = objects.Set()
-    world.thingRemBuffer = objects.Set()
+    world.partition = objects.Partition()
+
+    world.tokensBeingHit = setmetatable({--[[
+        [token] -> duration_of_hit
+    ]]}, {__mode="k"})
 end
 
 
 
-function world.addThing(thing)
-    assert(type(thing) == "table")
-    assert(thing.update)
-    assert(thing.type)
-    assert(thing.draw)
+function world.addEntity(ent)
+    assert(type(ent) == "table")
+    assert(ent.update)
+    assert(ent.type)
+    assert(ent.draw)
+
+    world.entities:addBuffered(ent)
 end
 
 
-function world.removeThing(thing)
+function world.addToken(tok)
+    assert(type(tok) == "table")
+    assert(tok.x and tok.y)
+    assert(tok.type)
+
+    world.entities:addBuffered(tok)
 end
+
+
+
+
+function world.removeEntity(ent)
+    world.entities:removeBuffered(ent)
+end
+
+
+function world.removeToken(tok)
+    world.tokens:removeBuffered(tok)
+end
+
 
 
 
 
 function world.draw()
-    drawGround()
+    -- drawGround()
 
-    drawTokens()
-    drawThings()
+    -- drawStuff()
+end
+
+
+
+function world.tokenExists(tok)
+    return world.tokens:has(tok)
+end
+
+function world.tryHitToken(tok)
+    local time = world.tokensBeingHit[tok]
+    if not time then
+        world.tokensBeingHit[tok] = 
+    end
+end
+
+
+
+---@class TokenPopulater: objects.Class
+local TokenList = objects.Class("world:TokenPopulater")
+
+function TokenList:init()
+    self.tokens = {}
+end
+
+---@param tokenId string
+---@param amount number?
+function TokenList:add(tokenId, amount)
+    self.tokens[tokenId] = (self.tokens[tokenId] or 0) + (amount or 1)
+end
+
+---@param tokenId string
+---@return number
+function TokenList:get(tokenId)
+    return self.tokens[tokenId] or 0
 end
 
 
 
 
+
 function world.update(dt)
-    for _, area in ipairs(self.areas) do
-        -- if area:
-        local objs = getOverlappingObjs(area)
-        for _,o in ipairs(objs) do
-            g.damage(o, 1)
-        end
+    world.entities:flush()
+    world.tokens:flush()
+
+    -- update TokenList
+    local tp = TokenList()
+    g.call("populateTokens", tp)
+
+    -- update partition
+    world.partition:clear()
+    for _, e in ipairs(world.entities) do
+        world.partition:add(e, e.x,e.y)
     end
+
+    for _, e in ipairs(world.entities) do
+        e:update(dt)
+    end
+
+    world.tokens:flush()
 end
 
 
