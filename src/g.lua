2e96c1807387c482ec822abe998aa5282e64f126
Oli <OliverWDGarrett@gmail.com>
2025-09-25 13:20:55 +1200

    Work on upgrades and upgrade-buying

--- src/g.lua
+++ src/g.lua
@@ -35,6 +35,10 @@ function g.getMainWorld()
     return currentSession.mainWorld
 end
 
+function g.getPrestige()
+    return currentSession.prestige
+end
+
 
 
 
@@ -357,6 +361,18 @@ g.stats.BoneLimit = g.defineStat("BoneLimit", 1000)
 ---@alias g.Resources {money: number, bones: number, rocks: number, logs: number}
 
 
+---@alias g.PrestigeRange {lower: integer, upper: integer}
+
+---@alias g.UpgradeInfo {id: string, prestige: number|g.PrestigeRange, x:number, y:number, image:string, price: g.Bundle}
+
+
+
+---@param prestige integer
+---@param range g.PrestigeRange
+function g.inPrestigeRange(prestige, range)
+    return (prestige >= range.lower) and (prestige <= range.upper)
+end
+
 
 ---@param a g.Bundle
 ---@param b g.Bundle
@@ -449,15 +465,26 @@ end
 
 
 
+
 ---@param price g.Bundle
 ---@return boolean
-function g.trySubtractResources(price)
+function g.canAfford(price)
     local r = currentSession.resources
-
     if (price.money or 0) > (r.money or 0) then return false end
     if (price.bones or 0) > (r.bones or 0) then return false end
     if (price.rocks or 0) > (r.rocks or 0) then return false end
     if (price.logs or 0) > (r.logs or 0) then return false end
+    return true
+end
+
+
+---@param price g.Bundle
+---@return boolean
+function g.trySubtractResources(price)
+    local r = currentSession.resources
+    if not g.canAfford(price) then
+        return false
+    end
 
     if price.money then r.money = r.money - price.money end
     if price.bones then r.bones = r.bones - price.bones end
