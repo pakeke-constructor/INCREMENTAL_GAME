cb4f3d2fc1f1d99310b5ca3b42fd53c0465aa292
Miku AuahDark <lumi.hotaru@gmail.com>
2025-09-26 10:04:10 +0800

    Remove UI scale override.

--- src/ui/ui.lua
+++ src/ui/ui.lua
@@ -121,26 +121,18 @@ local GLOBAL_SCALE_INCREMENT = 0.25
 local globalScaleTransform = love.math.newTransform()
 local globalScale = 1
 local gw, gh = 800, 600
-local globalScaleOverride = false
-
----@param gscale number
-local function updateGlobalScale(gscale)
-	globalScale = math.max(gscale, 1)
-	globalScaleTransform:reset():scale(globalScale)
-end
 
 local function updateGlobalScaleAutomatic()
-	if globalScaleOverride then
-		return
-	end
-
 	local w, h = love.graphics.getDimensions()
 	if w ~= gw or h ~= gh then
 		local wscale = w / 600
 		local hscale = h / 400
 		local scale = math.min(wscale, hscale)
 		local gscale = math.floor(scale / GLOBAL_SCALE_INCREMENT + 0.5) * GLOBAL_SCALE_INCREMENT
-		updateGlobalScale(gscale)
+		globalScale = math.max(gscale, 1)
+		globalScaleTransform:reset():scale(globalScale)
+		gw = w
+		gh = h
 	end
 end
 
@@ -159,18 +151,6 @@ end
 function ui.getUIScaling()
 	updateGlobalScaleAutomatic()
 	return globalScale
-end
-
----Override the UI scaling to specified scale number.
----@param scale number|nil
-function ui.overrideUIScaling(scale)
-	if scale then
-		globalScaleOverride = true
-		updateGlobalScale(scale)
-	else
-		globalScaleOverride = false
-		updateGlobalScaleAutomatic()
-	end
 end
 
 function ui.getScaledUIDimensions()
