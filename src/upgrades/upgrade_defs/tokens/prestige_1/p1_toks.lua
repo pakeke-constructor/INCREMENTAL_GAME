d55ca9b82b0d0956ff5816b527f8d787c7847134
Oli <OliverWDGarrett@gmail.com>
2025-10-01 13:56:13 +1300

    Created initial grass-objects

--- src/upgrades/upgrade_defs/tokens/prestige_1/p1_toks.lua
+++ src/upgrades/upgrade_defs/tokens/prestige_1/p1_toks.lua
@@ -1,15 +1,40 @@
 
 
-g.defineTokenUpgrade("basic_grass", "Basic Grass", {
+g.defineTokenUpgrade("grass_blades", "Grass Blades", {
     token = {
         resources = {money = 1},
-        maxHealth = 4
+        maxHealth = 5
     },
     upgrade = {
-        description = loc"hi",
         prestige = 0,
         price = {money=1},
         x=1,y=1,
+    }
+})
+
+
+g.defineTokenUpgrade("small_grass", "Small Grass", {
+    token = {
+        resources = {money = 2},
+        maxHealth = 8
+    },
+    upgrade = {
+        prestige = 0,
+        price = {money=5},
+        x=1,y=2,
+    }
+})
+
+
+g.defineTokenUpgrade("thick_grass", "Thick Grass", {
+    token = {
+        resources = {money = 4},
+        maxHealth = 15
+    },
+    upgrade = {
+        prestige = 0,
+        price = {money=100},
+        x=1,y=3,
     }
 })
 
