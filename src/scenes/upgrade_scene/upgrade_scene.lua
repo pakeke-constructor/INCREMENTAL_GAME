c01c574b88939838cf2ef29067c0f6b824ef6b94
Miku AuahDark <lumi.hotaru@gmail.com>
2025-10-02 08:56:05 +0800

    More code for the upgrade description.

--- src/scenes/upgrade_scene/upgrade_scene.lua
+++ src/scenes/upgrade_scene/upgrade_scene.lua
@@ -1,5 +1,6 @@
 
 local upgrades = require("src.upgrades.upgrades")
+local UpgradeDescription = require("src.ui.upgrades.upgrade_description_ui")
 
 
 local FreeCameraScene = require("src.scenes.FreeCameraScene")
@@ -8,7 +9,9 @@ local FreeCameraScene = require("src.scenes.FreeCameraScene")
 ---@class UpgradesScene: FreeCameraScene
 local upgscene = FreeCameraScene()
 
-
+---@type ui.UpgradeDescription
+upgscene.upgradeDescription = nil
+upgscene.upgradeDescriptionType = nil
 
 
 local TITLE = localization.localize("{o}UPGRADES!")
@@ -32,9 +35,37 @@ function upgscene:draw()
     g.getHUD():drawResourceHUD(self.camera)
 
     if hoveredUpgrade then
-        -- figure out if we render left or right.
+        if not self.upgradeDescription or upgscene.upgradeDescriptionType ~= hoveredUpgrade.type then
+            self.upgradeDescription = UpgradeDescription()
+            self.upgradeDescription:autoBuild(hoveredUpgrade)
+            self.upgradeDescriptionType = hoveredUpgrade.type
+        end
 
-        -- TODO: draw upgrade-hover-description here
+        local CONTENT_PADDING = 8
+        local r = Kirigami(0, 0, ui.getScaledUIDimensions())
+        local mx, my = ui.getMouse()
+        local descriptionBoxR = Kirigami(0, 0, self.upgradeDescription:getDimensions())
+            :padUnit(-CONTENT_PADDING) -- expand
+            :set(mx + 14, my - 3)
+            :clampInside(r:padUnit(4))
+
+        -- Background
+        love.graphics.setColor(0.2, 0.2, 0.4, 0.8)
+        love.graphics.rectangle("fill", descriptionBoxR:get())
+
+        -- Border
+        local lw = love.graphics.getLineWidth()
+        love.graphics.setColor(1,1,1)
+        love.graphics.setLineWidth(2)
+        love.graphics.setColor(0.,0.,0.08)
+        love.graphics.rectangle("line", descriptionBoxR:get())
+        love.graphics.setLineWidth(lw)
+
+        -- Upgrade description
+        love.graphics.setColor(1,1,1)
+        self.upgradeDescription:draw(descriptionBoxR.x + CONTENT_PADDING, descriptionBoxR.y + CONTENT_PADDING)
+    else
+        self.upgradeDescription = nil
     end
 
     ui.endUI()
--- src/ui/upgrades/upgrade_description_ui.lua
+++ src/ui/upgrades/upgrade_description_ui.lua
@@ -7,41 +7,110 @@ local UpgradeDescription = objects.Class("g:UpgradeDescription")
 
 local UPGRADE_DESC_MAX_WIDTH = 200
 
----@param uinfo g.UpgradeInfo
-function UpgradeDescription:init(uinfo)
+function UpgradeDescription:init()
     self.font = g.getSmallFont(16)
     self.titleFont = g.getBigFont(32)
 
     self.boxWidth = 100
 
-    ---@class g._UpgradeDescriptionElem
+    ---@class ui._UpgradeDescriptionElem
     ---@field package width number|nil
     ---@field package height number
     ---@field package render fun(x:number,y:number,w:number,h:number)
-    ---@type g._UpgradeDescriptionElem[]
+    ---@type ui._UpgradeDescriptionElem[]
     self.elements = {}
 
 end
 
 if false then
-    ---@param uinfo g.UpgradeInfo
     ---@return ui.UpgradeDescription
     ---@diagnostic disable-next-line: cast-local-type, missing-return
-    function UpgradeDescription(uinfo) end
+    function UpgradeDescription() end
 end
 
 
+
+---@param uinfo g.UpgradeInfo
+---@param level integer
+---@param nextLevel boolean? (Display next level values?)
+local function getUpgradeDescription(uinfo, level, nextLevel)
+    if not uinfo.description then
+        return ""
+    end
+
+    local currentValues = {uinfo:getValues(level)}
+    local nextValues = nil
+    if nextLevel then
+        nextValues = {uinfo:getValues(level + 1)}
+        assert(#currentValues == #nextValues)
+    end
+    local displayValue = {}
+    for i = 1, #currentValues do
+        local formatter = uinfo.valueFormatter[i]
+        local value
+
+        if type(formatter) == "string" then
+            if nextValues then
+                value = string.format(formatter.." -> "..formatter, currentValues[i], nextValues[i])
+            else
+                value = string.format(formatter, currentValues[i])
+            end
+        else
+            local val = formatter(currentValues[i])
+            if nextValues then
+                val = val.." -> "..formatter(nextValues[i])
+            end
+        end
+
+        displayValue[tostring(i)] = value
+    end
+
+    -- TODO: Clarify if the description should be passed to interpolator or it's already in localization.Interpolator
+    return loc(uinfo.description, displayValue)
+end
+
+
+---Create upgrade description automatically.
 ---@param uinfo g.UpgradeInfo
 function UpgradeDescription:autoBuild(uinfo)
     self:addTitle(uinfo.name, uinfo.image)
     self:addDivider()
 
     if uinfo.kind == "TOKEN" then
+        local tinfo = g.getTokenInfo(uinfo.tokenType or uinfo.type)
+
         -- Draw token info
+        ---@type [string, string][]
+        local defs = {}
+        for _, resId in ipairs(g.RESOURCE_LIST) do
+            if tinfo.resources[resId] then
+                defs[#defs+1] = {g.getResourceInfo(resId).image, "+"..g.formatNumber(tinfo.resources[resId])}
+            end
+        end
+
+        -- Lifetime always on 2nd column 1stfirst row
+        if #defs > 0 then
+            table.insert(defs, 2, {"health_icon", tostring(tinfo.maxHealth)})
+        else
+            defs[#defs+1] = {"health_icon", tostring(tinfo.maxHealth)}
+        end
+
+        self:addTextImageGrid(false, defs)
+        self:addDivider()
+
+        if tinfo.description then
+            self:addText(tinfo.description)
+            self:addDivider()
+        end
+    elseif uinfo.description then
+        local level = g.getUpgradeLevel(uinfo)
+        local realDesc = getUpgradeDescription(uinfo, math.max(level, 1), level > 0)
+        self:addText(realDesc)
+        self:addDivider()
     end
 
-    -- FIXME: Price is dynamic
-    self:addPrice(uinfo.price)
+    -- Just pick first price in resource for now
+    self:addPrice(g.getUpgradePrice(uinfo))
 end
 
 
@@ -82,14 +151,21 @@ function UpgradeDescription:addDivider()
 end
 
 
+---This centers the text
 ---@param txt string
-function UpgradeDescription:addText(txt)
+---@param align love.AlignMode?
+function UpgradeDescription:addText(txt, align)
     local stripped = richtext.stripEffects(txt)
     local fw, lines = self.font:getWrap(stripped, UPGRADE_DESC_MAX_WIDTH)
     local fh = self.font:getHeight() * #lines
+    align = align or "center"
 
-    return self:addBox(fw, fh, function(x,y,w,h)
-        richtext.printRich(txt, self.font, x,y, w, "center")
+    -- Update the box width
+    self.boxWidth = math.max(self.boxWidth, fw)
+    -- But respect the boxWidth dimension in case it's larger (so width is nil)
+    -- this is needed so that alignment other than "center" works.
+    return self:addBox(nil, fh, function(x,y,w,h)
+        richtext.printRich(txt, self.font, x,y, w, align)
     end)
 end
 
@@ -121,15 +197,17 @@ end
 
 ---@param imagefirst boolean
 ---@param defs {[1]:string,[2]:string}[] (first element is image, second element is text)
-function UpgradeDescription:addTextImageGrid(imagefirst, defs)
+---@param scale number?
+function UpgradeDescription:addTextImageGrid(imagefirst, defs, scale)
+    scale = scale or 1
     local rows = math.ceil(#defs / 2)
     local minwidth = self.boxWidth
 
     for i = 1, #defs, 2 do
-        local width = computeWidthOfImageText(self.font, defs[i][1], defs[i][2])
+        local width = computeWidthOfImageText(self.font, defs[i][1], defs[i][2]) * scale
 
         if defs[i + 1] then
-            width = width + 8 + computeWidthOfImageText(self.font, defs[i + 1][1], defs[i + 1][2])
+            width = width + 8 + computeWidthOfImageText(self.font, defs[i + 1][1], defs[i + 1][2]) * scale
         end
 
         minwidth = math.max(minwidth, width)
@@ -155,7 +233,7 @@ function UpgradeDescription:addTextImageGrid(imagefirst, defs)
         end
 
         for i, def in ipairs(defs) do
-            local cellR = rowsR[math.floor(i / 2)][(i - 1) % 2 + 1]
+            local cellR = rowsR[math.floor((i - 1) / 2) + 1][(i - 1) % 2 + 1]
             local rx, ry, rw, rh = cellR:get()
             local imageWidth = select(3, g.getImageQuad(def[1]):getViewport()) --[[@as number]]
             local textWidth = self.font:getWidth(richtext.stripEffects(def[2]))
@@ -176,6 +254,25 @@ end
 
 ---@param bundle g.Bundle
 function UpgradeDescription:addPrice(bundle)
+    -- TODO: Support more than 1 resource while keeping the "Price" text inline
+    -- It will be layouting nightmare though!
+    local res, value = next(bundle)
+    if res then
+        assert(value)
+        local resInfo = g.getResourceInfo(res)
+        local imageWidth = select(3, g.getImageQuad(resInfo.image):getViewport()) --[[@as number]]
+        self:addBox(nil, 16, function(x, y, w, h)
+            local priceR, resourceR = Kirigami(x, y, w, h):splitHorizontal(1, 1)
+
+            local px, py = priceR:get()
+            love.graphics.print("Price", self.font, px, py)
+
+            local rx, ry = resourceR:get()
+            g.drawImageOffset(resInfo.image, rx, ry, 0, 1, 1, 0, 0)
+            love.graphics.print(g.formatNumber(value), self.font, rx + imageWidth + 2, ry)
+        end)
+    end
+    --[[
     -- adds the price at the bottom
     local prices = {}
     for _, resId in ipairs(g.RESOURCE_LIST) do
@@ -184,13 +281,20 @@ function UpgradeDescription:addPrice(bundle)
         end
     end
     self:addTextImageGrid(true, prices)
+    ]]
 end
 
 
----@param x number
----@param y number
-function UpgradeDescription:draw(x, y)
-    
+---@param startx number
+---@param starty number
+function UpgradeDescription:draw(startx, starty)
+    local yoff = 0
+    for _, elem in ipairs(self.elements) do
+        local width = elem.width or self.boxWidth
+        local xoff = (self.boxWidth - width) / 2
+        elem.render(startx + xoff, starty + yoff, width, elem.height)
+        yoff = yoff + elem.height
+    end
 end
 
 function UpgradeDescription:getDimensions()
@@ -200,7 +304,7 @@ function UpgradeDescription:getDimensions()
         if elem.width then
             width = math.max(width, elem.width)
         end
-        height = math.max(height, elem.height)
+        height = height + elem.height
     end
 
     return width, height
