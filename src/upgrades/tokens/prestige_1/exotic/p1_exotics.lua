462151f9d8ae6f11fad05b119065cbd0fb33dd0c
Oli <OliverWDGarrett@gmail.com>
2025-10-03 15:46:00 +1300

    moved tok-defs

---
+++ src/upgrades/tokens/prestige_1/exotic/p1_exotics.lua
@@ -0,0 +1,18 @@
+
+--[[
+
+Exotic tokens.
+
+Have special effects:
+
+Green Mushroom (When destroyed, spawns 6 random grasses)
+Red Mushroom (When destroyed, explodes, dealing 10 dmg)
+Blue mushroom: When destroyed, spawns 4 lightning-bolts!
+
+Jungle cat: When hit, deals damage to surrounding grass tokens
+Clay Pot: gives a random amount of money, between 0 and 50
+Treasure chest: Gives random loot! (Between $50 and $500)
+Evil cat: Deal 10 damage to the nearest token every 0.4 seconds
+
+]]
+
