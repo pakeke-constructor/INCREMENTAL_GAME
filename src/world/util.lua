617307481a1eff3a07270055664577d8ea9372c2
Miku AuahDark <lumi.hotaru@gmail.com>
2025-10-13 11:47:56 +0800

    Revert "Add getUpgradeCoords helper to worldutil."
    This reverts commit d50d69e85d7d0cf8942a9156acc80ebca567309a.

--- src/world/util.lua
+++ src/world/util.lua
@@ -1,15 +1,6 @@
 local worldutil = {}
 
 
----@param x integer
----@param y integer
-function worldutil.getUpgradeCoords(x, y)
-    local size = consts.UPGRADE_IMAGE_SIZE
-    local spacing = consts.UPGRADE_GRID_SPACING + size
-    return x * spacing, y * spacing, size
-end
-
-
 ---@param x number
 ---@param y number
 ---@param rad number
