9872941f8199be7b2e3bdfc05d5277c93d781f91
Miku AuahDark <lumi.hotaru@gmail.com>
2025-10-13 15:42:43 +0800

    Add lightning cat token.

--- src/upgrades/tokens/prestige_1/exotic/p1_exotics.lua
+++ src/upgrades/tokens/prestige_1/exotic/p1_exotics.lua
@@ -16,3 +16,22 @@ Evil cat: Deal 10 damage to the nearest token every 0.4 seconds
 
 ]]
 
+-- TODO: Balancing
+g.defineTokenUpgrade("lightning_cat", "Lightning Cat", {
+    token = {
+        maxHealth = 70,
+        resources = {},
+        tokenDestroyed = function()
+            local world = g.getMainWorld()
+
+            for i = 1, 3 do
+                local x = love.math.random(world.WIDTH) - 1
+                local y = love.math.random(world.HEIGHT) - 1
+                worldutil.spawnLightning(x, y, 50)
+            end
+        end
+    },
+    upgrade = {
+        price = {money = 10000, logs = 1}
+    }
+})
