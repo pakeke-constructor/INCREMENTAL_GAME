e77135272c5e17301ac40ebcf4ac85329b57eb5f
Miku AuahDark <lumi.hotaru@gmail.com>
2025-10-13 10:15:09 +0800

    Farmer cat.

--- src/upgrades/misc/misc.lua
+++ src/upgrades/misc/misc.lua
@@ -1,3 +1,7 @@
+-------------------------
+-- Resource limit upgrade
+-------------------------
+
 ---@param id string
 ---@param name string
 ---@param resId g.ResourceType
@@ -31,6 +35,9 @@ defineResLimitUpgrade("bones_limit", "Bones Limit", "bones", {bones = 10}, 5)
 
 
 
+---------------------
+-- Capitalist upgrade
+---------------------
 g.defineUpgrade("capitalist", "Capitalist", {
     kind = "MISC",
     image = "money_particle_4",
@@ -45,4 +52,95 @@ g.defineUpgrade("capitalist", "Capitalist", {
     end,
     maxLevel = 4,
     price = {money = 500}
+})
+
+
+
+
+---------------------
+-- Farmer Cat upgrade
+---------------------
+
+---@class FarmerCatEntity: g.Entity
+---@field public dirX -1|1
+---@field public dirY -1|1
+---@field public speed number
+local FarmerCatEntity = {
+    image = "happy_cat",
+    radius = 20,
+    speed = 50
+}
+
+function FarmerCatEntity:update(dt)
+    local world = g.getMainWorld()
+
+    -- Update X position
+    self.x = self.x + self.speed * self.dirX * dt
+    if self.x < 0 then
+        self.x = -self.x -- make it positive
+        self.dirX = -self.dirX
+    elseif self.x >= world.WIDTH then
+        self.x = 2 * world.WIDTH - self.x
+        self.dirX = -self.dirX
+    end
+
+    -- Update Y position
+    self.y = self.y + self.speed * self.dirY * dt
+    if self.y < 0 then
+        self.y = -self.y -- make it positive
+        self.dirY = -self.dirY
+    elseif self.y >= world.HEIGHT then
+        self.y = 2 * world.HEIGHT - self.y
+        self.dirY = -self.dirY
+    end
+
+    -- Try harvest
+    world.tokenPartition:query(self.x,self.y, function (tok)
+        if math.distance(self.x-tok.x, self.y-tok.y) <= (self.radius + consts.HARVEST_AREA_LEEWAY) then
+            g.tryHitToken(tok)
+        end
+    end, self.radius)
+end
+
+
+local HARVEST_CIRCLE_INSIDE = {0.2,0.2,0.2,0.09}
+local HARVEST_CIRCLE_BORDER = {.9,.9,.9,0.8}
+
+function FarmerCatEntity:drawBelow()
+    local rad = self.radius
+    love.graphics.setColor(HARVEST_CIRCLE_INSIDE)
+    love.graphics.circle("fill", self.x, self.y, rad)
+
+    local lw = love.graphics.getLineWidth()
+    love.graphics.setLineWidth(math.floor(rad / 15))
+    love.graphics.setColor(HARVEST_CIRCLE_BORDER)
+    love.graphics.circle("line", self.x, self.y, rad)
+    love.graphics.setLineWidth(lw)
+end
+
+g.defineEntity("farmer_cat", FarmerCatEntity)
+
+-- TODO: Balancing
+g.defineUpgrade("farmer_cat", "Farmer Cat", {
+    kind = "MISC",
+    description = "Spawn %{1} Farmer Cat to farm for you.",
+    image = "happy_cat",
+    price = {money = 1000},
+    maxLevel = 10,
+    getEntityCount = function(uinfo, level)
+        return level
+    end,
+    getValues = function(uinfo, level)
+        return level
+    end,
+    spawnEntity = function()
+        local world = g.getMainWorld()
+        local x = love.math.random(0, world.WIDTH - 1)
+        local y = love.math.random(0, world.HEIGHT - 1)
+        local e = g.spawnEntity("farmer_cat", x, y)
+        ---@cast e FarmerCatEntity
+        e.dirX = love.math.random(0, 1) * 2 - 1
+        e.dirY = love.math.random(0, 1) * 2 - 1
+        return e
+    end
 })
