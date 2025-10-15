fab18a0d13e76ab4426c47b0fc94b87d8ae85858
Oli <OliverWDGarrett@gmail.com>
2025-10-15 16:34:29 +1300

    Added circle to SpinningAxe

--- src/entities/spinning_axe/spinning_axe.lua
+++ src/entities/spinning_axe/spinning_axe.lua
@@ -6,17 +6,25 @@ local SpinningAxe = {}
 SpinningAxe.lifetime = 3.6
 SpinningAxe.image = "spinning_axe"
 
+local RAD = 20
 
 function SpinningAxe:init()
-    local S=60
+    local S=40
     local a = love.math.random() * 2*math.pi
     self.vx = (S*math.cos(a))
     self.vy = (S*math.sin(a))
 
-    g.iterateTokensInArea(self.x,self.y, 40, function(tok)
+    g.iterateTokensInArea(self.x,self.y, RAD * 1.5, function(tok)
         g.damageToken(tok, 8)
     end)
 end
+
+function SpinningAxe:drawBelow()
+    love.graphics.setLineWidth(1)
+    love.graphics.setColor(1,1,1,0.4)
+    love.graphics.circle("line", self.x,self.y, RAD)
+end
+
 
 function SpinningAxe:update(dt)
     local ROT_SPEED = 8
@@ -27,7 +35,7 @@ end
 
 
 function SpinningAxe:perSecondUpdate()
-    g.iterateTokensInArea(self.x,self.y, 40, function(tok)
+    g.iterateTokensInArea(self.x,self.y, RAD, function(tok)
         g.damageToken(tok, 8)
     end)
 end
