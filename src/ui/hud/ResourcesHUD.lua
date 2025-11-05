14117bf05c08315913f0fc89887832b6da9e25f8
Oli <OliverWDGarrett@gmail.com>
2025-11-05 14:21:37 +1300

    changed particle speed

--- src/ui/hud/ResourcesHUD.lua
+++ src/ui/hud/ResourcesHUD.lua
@@ -18,7 +18,7 @@ Resources._resourceFont = love.graphics.newFont("assets/fonts/Smart 9h.ttf", 24,
 
 local SPAWN_ANIMATION_DURATION = 0.025
 local AFTERSPAWN_ANIMATION_DELAY = 0.06
-local PARTICLE_SPEED = 1000
+local PARTICLE_SPEED = 550
 local BEFOREHUD_TIME = SPAWN_ANIMATION_DURATION + AFTERSPAWN_ANIMATION_DELAY
 local RANDOM_DELAY = 0.25 -- Random delay before the particle is spawned.
 local PARTICLE_HUD_VISUAL_ATTENTION_DURATION = 0.3
