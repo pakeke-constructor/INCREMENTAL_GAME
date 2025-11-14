e44a3859980929e06d41d67acb45053a9ccf2bda
Miku AuahDark <lumi.hotaru@gmail.com>
2025-11-14 15:33:59 +0800

    Draw clouds in harvest area.

--- src/scenes/harvest_scene/harvest_scene.lua
+++ src/scenes/harvest_scene/harvest_scene.lua
@@ -2,6 +2,7 @@
 local lg=love.graphics
 
 local particles = require("src.modules.particles.particles")
+local cloudService = require(".cloud_service")
 
 
 
@@ -548,6 +549,12 @@ function harvest:draw()
 
     world:_draw()
 
+    -- Draw clouds
+    cloudService.drawShadow()
+    love.graphics.setColor(1, 1, 1, 0.67)
+    cloudService.draw()
+
+    love.graphics.setColor(1, 1, 1)
     self:_drawTokenStackAnim()
 
     self:resetCamera()
@@ -630,6 +637,9 @@ function harvest:update(dt)
         -- then it's gone.
         self.stackedTokenLerpTime = -1
     end
+
+    -- Update cloud
+    cloudService.update(dt, self.camera)
 
     if simulation.isSimulating() then
         simulation.update(dt)
