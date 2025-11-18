c5fb9af9881ab46ae5377ffbcb6636836516b595
Miku AuahDark <lumi.hotaru@gmail.com>
2025-11-18 13:55:00 +0800

    Cosmetic selection by category.

--- src/scenes/customization_scene/customization_scene.lua
+++ src/scenes/customization_scene/customization_scene.lua
@@ -23,6 +23,26 @@ local CATEGORY_COLOR = {
     [true] = {objects.Color.WHITE, objects.Color("#".."FF797568")},
     [false] = {objects.Color.TRANSPARENT, objects.Color.WHITE}
 }
+local SELECTED_COLOR = {
+    AVATAR = objects.Color("#".."FFDEBAE7"),
+    BACKGROUND = objects.Color("#".."FFB1D8EA"),
+    HAT = objects.Color("#".."FFEAB5B1"),
+}
+
+---@param categoryId integer
+local function getWornCosmeticId(categoryId)
+    local s = g.getSn()
+
+    if categoryId == 2 then
+        return s.avatar.avatar
+    elseif categoryId == 3 then
+        return s.avatar.background
+    elseif categoryId == 4 then
+        return s.avatar.hat
+    end
+
+    return nil
+end
 
 
 ---@class CustomizationScene: FreeCameraScene
@@ -40,6 +60,26 @@ function custom:init()
 end
 
 
+---@param cinfo g.CosmeticInfo
+local function isCosmeticSelected(cinfo)
+    local s = g.getSn()
+
+    if cinfo.type == "AVATAR" then
+        return s.avatar.avatar == cinfo.id
+    elseif cinfo.type == "BACKGROUND" then
+        return s.avatar.background == cinfo.id
+    elseif cinfo.type == "HAT" then
+        return s.avatar.hat == cinfo.id
+    end
+end
+
+---@type (fun(cinfo:g.CosmeticInfo):boolean)[]
+local filters = {
+    function(cinfo) return true end, -- All
+    function(cinfo) return cinfo.type == "AVATAR" end, -- Cats
+    function(cinfo) return cinfo.type == "BACKGROUND" end, -- Backgrounds
+    function(cinfo) return cinfo.type == "HAT" end, -- Hats
+}
 
 function custom:_drawUI()
     local r = Kirigami(0, 0, ui.getScaledUIDimensions())
@@ -85,6 +125,98 @@ function custom:_drawUI()
         end
         categoryR = categoryR:moveRatio(0, 1):moveUnit(0, CATEGORY_DIVIDER)
     end
+
+    -- Prepare text layout for the cosmetic name
+    local cosmeticNameR = Kirigami(0, 0, 32, 32)
+        :attachToLeftOf(categoryBaseR)
+        :attachToTopOf(categoryBaseR)
+        :moveRatio(0, 1)
+        :moveUnit(-8, 0)
+
+    -- Prepare cosmetic cell
+    local categoryfilter = filters[self.activeCategory]
+    local cellSize = COSMETIC_TILE_SIZE + COSMETIC_PADDING
+    local cellR = Kirigami(0, 0, cellSize, cellSize)
+        :attachToRightOf(cosmeticNameR)
+        :attachToBottomOf(cosmeticNameR)
+        :moveRatio(-COSMETIC_COLUMNS, 0)
+        :moveUnit(0, 8)
+    local cellBaseR = cellR -- for centering the profile HUD display later
+
+    -- Draw cosmetic cell by category
+    -- TODO: Scrollbar or a way to display more cosmetics later.
+    local column = 0
+    for _, v in ipairs(g.getUnlockedCosmetics()) do
+        local cinfo = g.getCosmeticInfo(v)
+        if categoryfilter(cinfo) then
+            column = column + 1
+
+            local clickableR = cellR:padUnit(COSMETIC_PADDING)
+            local selected = isCosmeticSelected(cinfo)
+
+            -- Draw  background
+            local alpha = selected and 1 or 0.1
+            love.graphics.setColor(helper.multiplyAlpha(SELECTED_COLOR[cinfo.type], alpha))
+            love.graphics.rectangle("fill", clickableR:get())
+
+            if iml.isHovered(clickableR:get()) then
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.rectangle("line", cellR:get())
+            end
+
+            local cx, cy = cellR:getCenter()
+            local s = cinfo.type == "BACKGROUND" and 1 or 2
+            love.graphics.setColor(cinfo.color)
+            assert(#cinfo.image > 0, cinfo.id)
+            g.drawImage(cinfo.image, cx, cy, 0, s * cinfo.upscale)
+
+            if iml.wasJustClicked(clickableR:get()) then
+                local s = g.getSn()
+                -- Select this avatar
+                if cinfo.type == "AVATAR" then
+                    s.avatar.avatar = cinfo.id
+                elseif cinfo.type == "BACKGROUND" then
+                    s.avatar.background = cinfo.id
+                elseif cinfo.type == "HAT" then
+                    if selected then
+                        s.avatar.hat = nil
+                    else
+                        s.avatar.hat = cinfo.id
+                    end
+                end
+            end
+
+            -- Advance next cell
+            cellR = cellR:moveRatio(1, 0)
+            if column >= COSMETIC_COLUMNS then
+                column = 0
+                cellR = cellR:moveRatio(-4, 1)
+            end
+        end
+    end
+
+    -- Draw category name and the selected cosmetic
+    local textDisplay = "{o}"..CATEGORIES[self.activeCategory][1].."{/o}"
+    if self.activeCategory > 1 then -- "all" category doesn't make sense to have this
+        local cosmeticid = getWornCosmeticId(self.activeCategory)
+
+        if cosmeticid then
+            local cinfo = g.getCosmeticInfo(cosmeticid)
+            textDisplay = helper.wrapRichtextColor(
+                SELECTED_COLOR[cinfo.type],
+                -- Why this? to make the outline in effect with the wavy
+                textDisplay.."{o} - {/o}{w}{o}"..cinfo.name.."{/o}{/w}"
+            )
+        end
+    end
+    love.graphics.setColor(1, 1, 1)
+    richtext.printRich(
+        textDisplay,
+        g.getSmallFont(32),
+        cosmeticNameR.x + cosmeticNameR.w - 1000,
+        cosmeticNameR.y,
+        1000, "right"
+    )
 end
 
 
