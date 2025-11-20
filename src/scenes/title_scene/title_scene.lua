2902e47e008e38ad41d4625da764fb6826a44196
Miku AuahDark <lumi.hotaru@gmail.com>
2025-11-20 08:57:50 +0800

    Separate title screen background to separate file.

--- src/scenes/title_scene/title_scene.lua
+++ src/scenes/title_scene/title_scene.lua
@@ -1,6 +1,5 @@
 local FreeCameraScene = require("src.scenes.FreeCameraScene")
-
-local TILE_SIZE = 64
+local titleBackground = require("src.titleBackground")
 
 local function init()
     g.gotoScene("map_scene")
@@ -14,26 +13,24 @@ local BUTTONS = {
 }
 local BUTTON_BASE_COL = objects.Color.GRAY
 local BUTTON_MAIN_COL = objects.Color.WHITE
-local BACKGROUND = objects.Color("#".."FFB3F024")
 
 ---@class TitleScene: FreeCameraScene
 local title = FreeCameraScene()
 
 function title:init()
     self.progress = 0
-    self.animatedTileCanvas = love.graphics.newCanvas(TILE_SIZE, TILE_SIZE)
 end
 
 ---@param dt number
 function title:update(dt)
     self.progress = (self.progress + dt * 0.2) % 1
-    self:_updateAnimationTile()
+    titleBackground.update(dt)
 end
 
 function title:draw()
     ui.startUI()
 
-    self:_drawBackground()
+    titleBackground.draw()
 
     -- Prepare layout
     local r = Kirigami(0, 0, ui.getScaledUIDimensions())
@@ -63,73 +60,5 @@ function title:draw()
     ui.endUI()
 end
 
-
-function title:_updateAnimationTile()
-    love.graphics.push("all")
-
-    love.graphics.origin()
-    love.graphics.setCanvas(self.animatedTileCanvas)
-    love.graphics.clear(1, 1, 1, 0)
-    love.graphics.setColor(1, 1, 1)
-
-    -- Draw cat
-    local catX = (1 - self.progress) * TILE_SIZE
-    local catY = (1 - self.progress) * TILE_SIZE
-    -- This double loop emulates wrapping
-    for oy = -1, 1 do
-        for ox = -1, 1 do
-            g.drawImage("happy_cat", catX + ox * TILE_SIZE, catY + oy * TILE_SIZE, -math.pi / 4, 1.5)
-        end
-    end
-
-    -- Draw text "CaT"
-    local text = "CaT"
-    local font = g.getSmallFont(32)
-    local tx = self.progress * TILE_SIZE
-    local ty = (1 - self.progress) * TILE_SIZE
-    local tw = font:getWidth(text)
-    local th = font:getHeight()
-    -- This double loop emulates wrapping
-    for oy = -1, 1 do
-        for ox = -1, 1 do
-            -- Ok the previous dual loop is for the corners of the canvas.
-            -- This one is to emulate "thick" text without {o} richtext.
-            for offx = -1, 1, 1 do
-                for offy = -1, 1, 1 do
-                    love.graphics.print(
-                        text,
-                        font,
-                        tx + ox * TILE_SIZE + offx,
-                        ty + oy * TILE_SIZE + offy,
-                        -math.pi / 4,
-                        1, 1,
-                        tw / 2, th / 2
-                    )
-                end
-            end
-        end
-    end
-
-    love.graphics.pop()
-end
-
-function title:_drawBackground()
-    love.graphics.clear(BACKGROUND)
-    --if true then return end
-
-    -- Draw canvas tiles
-    local uiW, uiH = ui.getScaledUIDimensions()
-    local tileW = math.ceil(uiW / TILE_SIZE)
-    local tileH = math.ceil(uiH / TILE_SIZE)
-    local tileOffX = (tileW * TILE_SIZE - uiW) / 2
-    local tileOffY = (tileH * TILE_SIZE - uiH) / 2
-
-    love.graphics.setColor(0, 0, 0, 0.3)
-    for y = 0, tileH - 1 do
-        for x = 0, tileW - 1 do
-            love.graphics.draw(self.animatedTileCanvas, x * TILE_SIZE - tileOffX, y * TILE_SIZE - tileOffY)
-        end
-    end
-end
 
 return title
---
+++ src/titleBackground.lua
@@ -0,0 +1,80 @@
+local TILE_SIZE = 64
+local BACKGROUND = objects.Color("#".."FFB3F024")
+
+local canvas = love.graphics.newCanvas(TILE_SIZE, TILE_SIZE)
+local time = love.math.random()
+
+local titleBackground = {}
+
+---@param dt number
+function titleBackground.update(dt)
+    time = (time + dt * 0.2) % 1
+
+    -- Update canvas
+    love.graphics.push("all")
+
+    love.graphics.origin()
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear(1, 1, 1, 0)
+    love.graphics.setColor(1, 1, 1)
+
+    -- Draw cat
+    local catX = (1 - time) * TILE_SIZE
+    local catY = (1 - time) * TILE_SIZE
+    -- This double loop emulates wrapping
+    for oy = -1, 1 do
+        for ox = -1, 1 do
+            g.drawImage("happy_cat", catX + ox * TILE_SIZE, catY + oy * TILE_SIZE, -math.pi / 4, 1.5)
+        end
+    end
+
+    -- Draw text "CaT"
+    local text = "CaT"
+    local font = g.getSmallFont(32)
+    local tx = time * TILE_SIZE
+    local ty = (1 - time) * TILE_SIZE
+    local tw = font:getWidth(text)
+    local th = font:getHeight()
+    -- This double loop emulates wrapping
+    for oy = -1, 1 do
+        for ox = -1, 1 do
+            -- Ok the previous dual loop is for the corners of the canvas.
+            -- This one is to emulate "thick" text without {o} richtext.
+            for offx = -1, 1, 1 do
+                for offy = -1, 1, 1 do
+                    love.graphics.print(
+                        text,
+                        font,
+                        tx + ox * TILE_SIZE + offx,
+                        ty + oy * TILE_SIZE + offy,
+                        -math.pi / 4,
+                        1, 1,
+                        tw / 2, th / 2
+                    )
+                end
+            end
+        end
+    end
+
+    love.graphics.pop()
+end
+
+function titleBackground.draw()
+    love.graphics.clear(BACKGROUND)
+
+    -- Draw canvas tiles
+    local uiW, uiH = ui.getScaledUIDimensions()
+    local tileW = math.ceil(uiW / TILE_SIZE)
+    local tileH = math.ceil(uiH / TILE_SIZE)
+    local tileOffX = (tileW * TILE_SIZE - uiW) / 2
+    local tileOffY = (tileH * TILE_SIZE - uiH) / 2
+
+    love.graphics.setColor(0, 0, 0, 0.3)
+    for y = 0, tileH - 1 do
+        for x = 0, tileW - 1 do
+            love.graphics.draw(canvas, x * TILE_SIZE - tileOffX, y * TILE_SIZE - tileOffY)
+        end
+    end
+end
+
+return titleBackground
