9287243c6689551bd4ccfd6b383d4427b8f8c6c0
Oli <OliverWDGarrett@gmail.com>
2025-11-27 20:28:15 +1300

    work on rewards

--- src/rewards/rewards.lua
+++ src/rewards/rewards.lua
@@ -9,20 +9,21 @@ PLANNING:
 
 TENETS:
 
-- We want relics to *feel* exciting.
+- We want rewards to *feel* exciting.
 - Decisions should feel somewhat meaningful.
-- Have proc-genned relics.
+- Have proc-genned rewards.
 
 
 HOW TO ACHIEVE:
-- Hardcoded early-game relics?
-- "tiers" or rarity for relics?
+- Hardcoded early-game rewards?
+- "tiers" or rarity for rewards?
 
 
-RELIC TYPES:
+REWARD TYPES:
 - COMMON: +Resource bundle
 - COMMON: Simple permanent stat increase
 - COMMON: Grants temporary effect (g.grantEffect)
+- RARE: Grants a bunch of stacked-tokens that give BIG rewards :)
 - RARE: Exotic permanent stat increase:
     - Deal +1 damage to grass-tokens
     - When lightning strikes, 
@@ -38,11 +39,53 @@ HOW SHOULD WE DO THE UI?
 
 ]]
 
-local relics = {}
+local rewards = {}
 
 
-function relics.generateRandomRelics()
-    -- generates 3 random relics to choose from
+---@alias g.Reward.Type "TOKEN"|"RESOURCE"|"STAT_BUFF"|""
+
+---@class g.Reward
+---
+---@field token g.TokenInfo? only for token-rewards
+---
+---@field resources g.Bundle? only for resource-rewards
+---
+---@field effect g.EffectInfo? only for effect-rewards
+---@field effectDuration number? (also effect-rewards)
+---
+---@field stackedToken g.TokenInfo? (also effect-rewards)
+---@field stackedTokenCount number?
+---
+---@field description string?
+---
+---@field icon string
+local Reward = {}
+
+
+
+---@param rew g.Reward
+local function assertRewardIsValid(rew)
+    local ct = 0
+    if rew.resources then ct = ct + 1 end
+    if rew.effect then ct = ct + 1 end
+    if rew.token then ct = ct + 1 end
+    if rew.description then ct = ct + 1 end
+    if rew.stackedToken then ct = ct + 1 end
+    assert(ct == 1, "Invalid reward: Rewards need to be exactly ONE type")
+
+    if rew.effect then
+        assert(rew.effectDuration, "Effects need a duration")
+    end
+    if rew.stackedToken then
+        assert(rew.stackedTokenCount, "stackedToken rewards need a count")
+    end
+
+    assert(rew.icon)
+end
+
+
+function rewards.generateRandomRewards()
+    -- generates 3 random rewards to choose from
     local sn=g.getSn()
     if g.getPrestige() == 0 then
         if sn.level < 4 then
@@ -50,20 +93,50 @@ function relics.generateRandomRelics()
         end
     end
 
-    return relicList
+    local rewardList = {
+        -- resource-reward
+        -- effect-reward
+        -- OTHER-reward
+    }
+    helper.shuffle(rewardList)
+    return rewardList
 end
 
 
 
-function relics.drawRelicDescription(rel, x,y,w,h)
-    -- used for relic-selection,
-    -- AND used for 
+
+---@param rew g.Reward
+---@param x any
+---@param y any
+---@param w any
+---@param h any
+function rewards.drawRewardDescription(rew, x,y,w,h)
+    -- used for reward-selection,
+    -- AND used for reward-UI on HUD
 
 end
 
 
-function relics.addRelic()
-    return relic
+
+---@param rew g.Reward
+function rewards.selectReward(rew)
+    assertRewardIsValid(rew)
+
+    if rew.resources then
+        g.addResources(rew.resources)
+    elseif rew.effect then
+        assert(rew.effectDuration)
+        g.grantEffect(rew.effect.type, rew.effectDuration)
+    elseif rew.stackedToken then
+        for _=1, rew.stackedTokenCount do
+            local w,h = ui.getScaledUIDimensions()
+            local sx,sy = w/2 + love.math.random(-100,100), h/2 + love.math.random(-100,100)
+            g.stackToken(rew.stackedToken.type, sx,sy)
+        end
+    else
+
+    end
 end
 
 
+return rewards
