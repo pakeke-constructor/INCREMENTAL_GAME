d51207a054d9dcd9deb457106c4f1d2f53f73f54
Oli <OliverWDGarrett@gmail.com>
2025-11-27 14:24:26 +1300

    work; no luck

--- tooling/webscraper.py
+++ tooling/webscraper.py
@@ -2,12 +2,12 @@
 import requests
 import time
 
-def scrape_top_posts(subreddit, limit=100, time_sleep=1):
+def scrape_top_posts(subreddit, limit=100, sleep=1):
     posts = []
     after = None
     
     while len(posts) < limit:
-        time.sleep(time_sleep)
+        time.sleep(sleep)
         url = f"https://www.reddit.com/r/{subreddit}/top.json"
         params = {'limit': min(100, limit - len(posts)), 't': 'all'}
         if after:
@@ -36,29 +36,15 @@ def scrape_top_posts(subreddit, limit=100, time_sleep=1):
     return posts
 
 
-
-def get_comments(url, time_sleep=1):
-    time.sleep(time_sleep)
-
-    r = requests.get(
-        url,
-        headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'},
-        params={'raw_json':1}
-    )
-
-    print(r)
-
+def get_comments(url, sleep=1):
+    r = requests.get(url, headers={'User-Agent': 'script'})
     data = r.json()
-    print("DATA:",data)
-    print("\n\n")
     comments = []
     
     def extract(items):
         for item in items:
             if item['kind'] == 't1':
                 c = item['data']
-                print("KEYS",c.keys())
-                return
                 comments.append({'author': c['author'], 'body': c['body']})
                 if c.get('replies'):
                     extract(c['replies']['data']['children'])
@@ -69,19 +55,22 @@ def get_comments(url, time_sleep=1):
 
 
 
-
 def scrape_incremental_sentiment():
     subreddit = "incremental_games"
     posts = scrape_top_posts(subreddit, limit=10)
+
+    time.sleep(1)
 
     for i, post in enumerate(posts[:10], 1):
         upvotes = post["upvotes"]
         title = post["title"]
         url = post["url"]
 
-        all_comments = get_comments(url)
+        all_comments = get_comments(url + ".json", sleep=2)
         for c in all_comments:
             print(f"{c['author']}: {c['body']}\n")
+
+        print("COMMENTS DONE DONE!")
 
 
 
