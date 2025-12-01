382eae59e7c44731adc7dbe7b996f0f45c93585d
Miku AuahDark <lumi.hotaru@gmail.com>
2025-12-01 09:46:34 +0800

    New buttons on title screen.

--- src/scenes/title_scene/title_scene.lua
+++ src/scenes/title_scene/title_scene.lua
@@ -14,14 +14,34 @@ local function init()
     g.gotoScene("map_scene")
 end
 
-local BUTTONS = {
-    {"Play", init},
-    {"Settings", function() g.gotoScene("setting_scene") end},
-    {"Stats", function() end},
-    {"Quit", love.event.quit},
-}
 local BUTTON_BASE_COL = objects.Color("#" .. "FF9F14F6")
 local BUTTON_MAIN_COL = objects.Color("#" .. "FF3B12A4")
+local PLAYB_BASE_COL = objects.Color("#" .. "FFE0AC35")
+local PLAYB_MAIN_COL = objects.Color("#" .. "FF5E4200")
+local SECONDARY_BUTTONS = {
+    {
+        loc"Settings",
+        BUTTON_BASE_COL,
+        BUTTON_MAIN_COL,
+        function() g.gotoScene("setting_scene") end
+    },
+    {
+        loc"Stats",
+        BUTTON_BASE_COL,
+        BUTTON_MAIN_COL,
+        function() end
+    },
+    {
+        loc"Quit",
+        objects.Color("#".."FFF26957"),
+        objects.Color("#".."FF4E0E05"),
+        love.event.quit
+    },
+}
+
+local text = {
+    play = "{w amp=0.5 freq=0.7}"..loc("Play").."{/w}"
+}
 
 ---@class TitleScene: FreeCameraScene
 local title = FreeCameraScene()
@@ -35,6 +55,10 @@ function title:update(dt)
     self.progress = (self.progress + dt * 0.2) % 1
     titleBackground.update(dt)
 end
+
+local PRIMARY_BUTTON_SIZE = {200, 80}
+local SECONDARY_BUTTON_SIZE = {144, 40}
+local BUTTON_PAD = 4
 
 function title:draw()
     ui.startUI()
@@ -54,16 +78,31 @@ function title:draw()
     -- titleAreaR = titleAreaR:set(nil, nil, width, height):center(topR)
     -- richtext.printRich(TITLE_TEXT, titleFont, titleAreaR.x, titleAreaR.y, titleAreaR.w, "center")
 
-    -- Draw buttons
-    local buttonGrid = Kirigami(0, 0, 144, 40 * #BUTTONS)
+    -- Calculate button layout size
+    local buttonHeights = PRIMARY_BUTTON_SIZE[2] + SECONDARY_BUTTON_SIZE[2] * #SECONDARY_BUTTONS
+    local maxButtonR = Kirigami(0, 0, ui.getScaledUIDimensions(), buttonHeights)
         :center(r)
-        :grid(1, #BUTTONS)
-    for i, binfo in ipairs(BUTTONS) do
-        local buttonPadR = buttonGrid[i]:padUnit(4)
+
+    -- Draw buttons
+    local playButtonR = Kirigami(0, 0, unpack(PRIMARY_BUTTON_SIZE))
+        :centerX(maxButtonR)
+        :attachToTopOf(maxButtonR)
+        :moveRatio(0, 1)
+    local secondaryButtonGrid = Kirigami(0, 0, SECONDARY_BUTTON_SIZE[1], SECONDARY_BUTTON_SIZE[2] * #SECONDARY_BUTTONS)
+        :attachToBottomOf(playButtonR)
+        :centerX(playButtonR)
+        :grid(1, #SECONDARY_BUTTONS)
+
+    if ui.Button(text.play, PLAYB_BASE_COL, PLAYB_MAIN_COL, playButtonR:padUnit(BUTTON_PAD)) then
+        init()
+    end
+
+    for i, binfo in ipairs(SECONDARY_BUTTONS) do
+        local buttonPadR = secondaryButtonGrid[i]:padUnit(4)
         love.graphics.setColor(0, 0, 0)
 
-        if ui.Button(helper.wrapRichtextColor(objects.Color.WHITE, binfo[1]), BUTTON_MAIN_COL, BUTTON_BASE_COL, buttonPadR) then
-            binfo[2]()
+        if ui.Button(helper.wrapRichtextColor(objects.Color.WHITE, binfo[1]), binfo[2], binfo[3], buttonPadR) then
+            binfo[4]()
         end
     end
 
