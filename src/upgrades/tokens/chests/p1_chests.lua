c044f492b89dc386295323417a6c79bd9823e338
Oli <OliverWDGarrett@gmail.com>
2025-12-02 19:51:22 +1300

    Adjusted chest sprites per palette

--- src/upgrades/tokens/chests/p1_chests.lua
+++ src/upgrades/tokens/chests/p1_chests.lua
@@ -22,3 +22,22 @@ g.defineToken("chest_golden", "Golden Chest", {
     maxHealth = 20,
     resources = {money = 100},
 })
+
+
+
+g.defineToken("chest_fish", "Fishy Chest", {
+    maxHealth = 7,
+    resources = {fish = 10},
+})
+g.defineToken("chest_fabric", "Fabric Chest", {
+    maxHealth = 7,
+    resources = {fabric = 10},
+})
+g.defineToken("chest_juice", "Juice Chest", {
+    maxHealth = 7,
+    resources = {juice = 10},
+})
+g.defineToken("chest_bread", "Bread Chest", {
+    maxHealth = 7,
+    resources = {bread = 10},
+})
