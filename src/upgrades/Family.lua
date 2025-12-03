5ddc9cb78b4f141d70b7bd782819afc58d144ad9
Oli <OliverWDGarrett@gmail.com>
2025-12-03 23:32:14 +1300

    commit

---
+++ src/upgrades/Family.lua
@@ -0,0 +1,22 @@
+
+-- TODO:
+-- do we even need this?
+
+
+---@class g.Family
+local Family = objects.Class("g:Family")
+
+
+function Family:init()
+
+end
+
+
+function Family:add(upgradeId)
+
+end
+
+
+
+return Family
+
