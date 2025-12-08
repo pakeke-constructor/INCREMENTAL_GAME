400c1743cd192877b79da564e1297227947c1c70
Oli <OliverWDGarrett@gmail.com>
2025-12-08 14:13:09 +1300

    added shiny-diamond

--- src/upgrades/xp/xp.lua
+++ src/upgrades/xp/xp.lua
@@ -1,13 +1,13 @@
 
 
 
-g.defineUpgrade("xp_diamond", "Experienced Diamond", {
+g.defineUpgrade("shiny_diamond", "Shiny Diamond", {
     description = "+%{1}% Experience gain!",
     kind = "MISC",
 
     getValues = helper.percentageGetter(5, 5),
 
-    getXPMultiplier = function(self, level)
+    getXpMultiplierMultiplier = function(self, level)
         local a=self:getValues(level)
         return 1+(a/100)
     end
