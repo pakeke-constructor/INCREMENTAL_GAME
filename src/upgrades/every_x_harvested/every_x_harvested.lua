bc71250580595772c40ec41892e7853ef8bbc6db
Oli <OliverWDGarrett@gmail.com>
2025-12-08 16:07:56 +1300

    work on every-x upgrades

--- src/upgrades/every_x_harvested/every_x_harvested.lua
+++ src/upgrades/every_x_harvested/every_x_harvested.lua
@@ -14,14 +14,31 @@ end
 
 ---@class _.every_x_ACTION
 local ACTIONS = {
-    {id = "lightning", name="Lightning",description="spawn lightning!", func = function(tok)
-        worldutil.spawnLightning(tok.x,tok.y, 2,6)
+    {id = "lightning", name="Lightning", description="spawn lightning!", func = function(tok)
+        worldutil.spawnLightning(tok.x, tok.y, 2, 6)
     end},
-    {id = "bombs", name="Bombs",description="spawn a bomb!"},
-    {id = "chests", name="Chests",description="spawn a random chest!"},
-    {id = "money_bonus", name="Bonuses",description="earn +10 {money}"},
-    {id = "xp_bonus", name="Experience",description="earn +10 experience"},
+    {id = "bombs", name="Bombs", description="spawn a bomb!", func = function(tok)
+        g.spawnToken("bomb",tok.x,tok.y)
+    end},
+    {id = "chests", name="Chests", description="spawn a random chest!", func = function(tok)
+        local x,y = g.getRandomPositionForToken()
+        if x and y then
+            if love.math.random() < 0.5 then
+                g.spawnToken("chest_big", x,y)
+            else
+                g.spawnToken("chest_small", x,y)
+            end
+        end
+    end},
+    {id = "money_bonus", name="Bonuses", description="earn +10 {money}", func = function(tok)
+        g.addResource("money", 10)
+    end},
+    {id = "xp_bonus", name="Experience", description="earn +10 experience", func = function(tok)
+        local sn = g.getSn()
+        sn.xp = sn.xp + 10
+    end},
 }
+
 
 ---@class _.every_x_CATEGORY
 local CATEGORIES = {
@@ -34,9 +51,10 @@ local CATEGORIES = {
 for _,action in ipairs(ACTIONS) do
     for _,category in pairs(CATEGORIES) do
         local id = "every_x_" .. category.id .. "_do_" .. action.id
-        local name =  category.name .. action.name -- eg:  "Mushroom Bombs"
+        local name = category.name .. action.name -- eg:  "Mushroom Bombs"
         defEveryXUpgrade(id, name, {
-            image = action.image,
+            image = "null_image",
+
             kind = "HARVESTING",
 
             description = ("Every %{1} %s harvested, "):format(category.plural) .. action.description,
@@ -47,6 +65,9 @@ for _,action in ipairs(ACTIONS) do
                         action.func(tok)
                     end
                 end
+            end,
+            drawUI = function(uinfo, level, x, y, w, h)
+                -- todo: draw orbiting shit here?
             end
         })
     end
