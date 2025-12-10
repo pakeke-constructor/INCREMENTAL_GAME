8cb27e98b980b546381d73e0cf8293a015d117b5
Miku AuahDark <lumi.hotaru@gmail.com>
2025-12-10 09:16:42 +0800

    Remove slime bucket rotation.

--- src/upgrades/orbits/orbits.lua
+++ src/upgrades/orbits/orbits.lua
@@ -75,9 +75,6 @@ defOrbitalUpgrade("orbital_scythe", "Orbital Scythe", {
 g.defineEntity("slime_bucket", {
     image = "slime_bucket",
     orbitRing = 2,
-    update = function(ent, dt)
-        ent.rot = ((ent.rot or 0) + dt) % (2 * math.pi)
-    end,
     hitToken = {
         radius = 24,
         collision = function(_, tok)
