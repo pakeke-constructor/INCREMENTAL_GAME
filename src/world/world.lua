6b221239c2c836e00f8fd762293373658cda7e98
Oli <OliverWDGarrett@gmail.com>
2025-12-10 13:26:51 +1300

    woops

--- src/world/world.lua
+++ src/world/world.lua
@@ -316,8 +316,8 @@ local function drawToken(tok)
     end
     if tok.starred then
         local s = math.sin(love.timer.getTime()*4 + tok.id*4.143)
-        local sx = math.sin(love.timer.getTime()*8 + tok.id*4.143)
-        g.drawImage("star_visual", tok.x-6,tok.y-5+s, 0, sx,1)
+        local sc = math.sin(love.timer.getTime()*8 + tok.id*4.143)
+        g.drawImage("star_visual", tok.x-6,tok.y-5+s, 0, sc,1)
     end
     tok.starred = true
 
