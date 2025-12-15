42a517ea0bb353f0f108fdec643ab96ea6c32f6c
Oli <OliverWDGarrett@gmail.com>
2025-12-15 14:05:25 +1300

    gave berries names

--- src/upgrades/tokens/berries/berries.lua
+++ src/upgrades/tokens/berries/berries.lua
@@ -100,11 +100,24 @@ local function makeId(berry, i)
 end
 
 
+local STALK_NAMES = {
+    "seedling",
+    "patch",
+    "sprout",
+    "bush",
+    "vine",
+}
+
+local function makeName(berry, i)
+    return berry.name .. " " .. STALK_NAMES[i]
+end
+
+
 for _, berry in ipairs(BERRIES) do
     -- define berry-tokens:
     for i=1, 5 do
         local token_id = makeId(berry,i)
-        local name = berry.name .. " ("..tostring(i)..")"
+        local name = makeName(berry,i)
 
         local mult = RESOURCE_MULTIPLIERS[i]
 
@@ -117,7 +130,10 @@ for _, berry in ipairs(BERRIES) do
 
         local desc = nil
         if DEPOPULATE_TOKEN[i] then
-            desc = ("{c r=1 g=0.2 b=0.2}{o}Removes tier-%s {%s} crops!"):format(DEPOPULATE_TOKEN[i], berry.id)
+            desc = ("{c r=1 g=0.2 b=0.2}{o}Removes %s {%s} crops!"):format(
+                makeName(berry, DEPOPULATE_TOKEN[i]),
+                berry.id
+            )
         end
 
         local stalk_id = "stalk_"..tostring(i)
