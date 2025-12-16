9f1dd30a77b87a2480b3719fe8926af1ada6caeb
Oli <OliverWDGarrett@gmail.com>
2025-12-16 20:53:46 +1300

    removed dev cap

--- src/g.lua
+++ src/g.lua
@@ -718,7 +718,7 @@ end
 g.defineResource("money", {
     image="money",
     limitStat="MoneyLimit",
-    startingLimit=(consts.DEV_MODE and 10000000000000) or 1000,
+    startingLimit=1000,
     color = {0.71, 0.55, 0.02},
 })
 g.defineResource("juice", {
