d7a0bbf24be5407b223aeb431a08e9aac382f592
Miku AuahDark <lumi.hotaru@gmail.com>
2025-12-17 10:22:16 +0800

    Use helper.tooltip for potion description.

--- src/scenes/harvest_scene/harvest_scene.lua
+++ src/scenes/harvest_scene/harvest_scene.lua
@@ -124,12 +124,10 @@ local EFFECT_COLORS = {
     [true] = {
         BG = objects.Color("#".."FF592404"),
         FG = objects.Color("#".."FFcF280E"),
-        DESC = objects.Color("#".."FFF4AEAB")
     },
     [false] = {
         BG = objects.Color("#".."FF1C4A1C"),
         FG = objects.Color("#".."FF75D963"),
-        DESC = objects.Color("#".."FF79BBDF")
     },
 }
 
@@ -141,11 +139,11 @@ function harvest:_drawActiveEffects()
         :moveUnit(-8, 0)
 
     local font = g.getSmallFont(16)
+    local tooltipDrawn = nil
     for eff, duration in g.getMainWorld():_iterateActiveEffects() do
         local effInfo = g.getEffectInfo(eff)
         local bgcolor = EFFECT_COLORS[effInfo.isDebuff].BG
         local fgcolor = EFFECT_COLORS[effInfo.isDebuff].FG
-        local desccolor = EFFECT_COLORS[effInfo.isDebuff].DESC
 
         -- Draw icon
         local x, y = effectIconR:getCenter()
@@ -177,59 +175,18 @@ function harvest:_drawActiveEffects()
         )
 
         if iml.isHovered(effectIconR:get()) then
-            -- Calculate description info data for drawing
-            local bigFont = g.getBigFont(16)
-            local titleWidth = bigFont:getWidth(richtext.stripEffects(effInfo.name))
             local description = effInfo.description or ""
-            local width, lines = font:getWrap(
-                richtext.stripEffects(effInfo.description or ""),
-                math.max(titleWidth, 200)
-            )
-            local height = bigFont:getHeight() + 8 + #lines * font:getHeight()
-
-            -- Draw description
-            local PADDING = 4
-            local descWidth = 2 * PADDING + width
-            local descHeight = 2 * PADDING + height
-            local descX = effectIconR.x - descWidth - 4
-            local descY = effectIconR.y + effectIconR.h
-            love.graphics.setColor(helper.multiplyAlpha(bgcolor, 0.7))
-            love.graphics.rectangle("fill", descX, descY, descWidth, descHeight)
-            love.graphics.setColor(fgcolor)
-            love.graphics.rectangle("line", descX, descY, descWidth, descHeight)
-
-            local yoff = 0
-            love.graphics.setColor(1, 1, 1)
-            richtext.printRich(
-                "{o}"..effInfo.name.."{/o}",
-                bigFont,
-                descX + PADDING,
-                descY + PADDING,
-                width,
-                "right"
-            )
-            yoff = yoff + bigFont:getHeight()
-            love.graphics.setColor(1, 1, 1, 0.7)
-            love.graphics.line(
-                descX + PADDING + 4,
-                descY + PADDING + yoff + 4,
-                descX + descWidth - PADDING - 4,
-                descY + PADDING + yoff + 4
-            )
-            yoff = yoff + 8
-            love.graphics.setColor(desccolor)
-            richtext.printRich(
-                "{o}"..description.."{/o}",
-                font,
-                descX + PADDING,
-                descY + PADDING + yoff,
-                width,
-                "right"
-            )
+            if #description > 0 then
+                tooltipDrawn = {description = description, x = effectIconR.x, y = effectIconR.y + effectIconR.h}
+            end
         end
 
         -- Next
         effectIconR = effectIconR:moveRatio(0, 1):moveUnit(0, 4)
+    end
+
+    if tooltipDrawn then
+        helper.tooltip(tooltipDrawn.description, tooltipDrawn.x, tooltipDrawn.y, 1, 0)
     end
 end
 
