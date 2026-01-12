782c96c02cfa7b13e69dde13ede8e6df9699af37
Oli <OliverWDGarrett@gmail.com>
2026-01-12 15:54:14 +1300

    vibe coded lightWorld

--- src/scenes/boss_scene/boss_scene.lua
+++ src/scenes/boss_scene/boss_scene.lua
@@ -1,4 +1,3 @@
-
 local FreeCameraScene = require("src.scenes.FreeCameraScene")
 local vignette = require("src.modules.vignette.vignette")
 
@@ -8,14 +7,19 @@ local vignette = require("src.modules.vignette.vignette")
 local boss = FreeCameraScene()
 
 
-local STATUE_X, STATUE_Y = 100, 200
-local DOOR_X, DOOR_Y = 400, 300
+local STATUE_X, STATUE_Y = 536, 188
+local DOOR_X, DOOR_Y = 90, 192
+
+local lights = {
+    {x=100, y=100, size=2},
+    {x=200, y=300, size=3}
+}
 
 
 function boss:init()
     self.background = lg.newImage("src/scenes/boss_scene/challengeroom.png")
-    self.statue = lg.newImage("src/scenes/boss_scene/challengeroom_statue")
-    self.door = lg.newImage("src/scenes/boss_scene/challengeroom_hallway")
+    self.statue = lg.newImage("src/scenes/boss_scene/challengeroom_statue.png")
+    self.door = lg.newImage("src/scenes/boss_scene/challengeroom_hallway.png")
 end
 
 
@@ -42,6 +46,9 @@ function boss:draw()
     local y = (h - scaledH) / 2
 
     love.graphics.draw(self.background, x, y, 0, scale, scale)
+    
+    love.graphics.draw(self.statue, x + STATUE_X * scale, y + STATUE_Y * scale, 0, scale, scale)
+    love.graphics.draw(self.door, x + DOOR_X * scale, y + DOOR_Y * scale, 0, scale, scale)
 
 
     ui.startUI()
