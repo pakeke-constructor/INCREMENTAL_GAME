e06fa2138b73d33f2ac1dd4130ffd5be0c1d3ec3
Miku AuahDark <lumi.hotaru@gmail.com>
2026-01-22 12:24:51 +0800

    Revert debug hotkey button behavior.

--- src/scenes/harvest_scene/harvest_scene.lua
+++ src/scenes/harvest_scene/harvest_scene.lua
@@ -1180,8 +1180,8 @@ function harvest:keyreleased(k)
     elseif consts.DEV_MODE then
         if k=="1" then
             --openBossPopup(self)
-            g.summonBoss("pumpkin_boss")
-            --g.incrementPrestige()
+            --g.summonBoss("pumpkin_boss")
+            g.incrementPrestige()
         elseif k=="2" then
             local tok = helper.randomChoice(g.TOKEN_LIST)
             for _ = 1, love.math.random(1, 15) do
