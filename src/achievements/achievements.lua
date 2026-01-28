0e61b08cda1105cb8a7d79a6b39a087ce2aadc54
Oli <OliverWDGarrett@gmail.com>
2026-01-28 15:56:26 +1300

    added achievement images

--- src/achievements/achievements.lua
+++ src/achievements/achievements.lua
@@ -5,19 +5,18 @@
 ACHIVEMENTS:
 ======================
 
-Earn over $100!
-Earn over $10000!
-Unlock slime upgrade!
-Spawn a knife-cat!
-Kill a boss!
-Spend over $5000 on one upgrade
-Spend over $20000 on one upgrade
-Catch a fish!
-Harvest 100 crops!
-Harvest 1000 crops!
-
-
-
+rich_1: Earn over $1000!
+rich_2: Earn over $10000!
+slime: Unlock slime upgrade!
+knife_cat: Spawn a knife-cat!
+slayer: Kill a boss!
+levelup: Reach level 30
+merchant_1: Spend over $5000 on one upgrade
+merchant_2: Spend over $20000 on one upgrade
+fishercat: Catch a fish!
+harvester_1: Harvest 500 crops!
+harvester_2: Harvest 5000 crops!
+delegation: Unlock farmer-cat upgrade!
 
 ]]
 
@@ -34,7 +33,16 @@ function achievements.defineAchievement(id, img)
 end
 
 
+
+-- simple runtime caching, avoids overhead of steam API
+local UNLOCKED_ACHIEVEMENT_RUNTIME_CACHE = {}
+
+
 function achievements.unlockAchievement(id)
+    if UNLOCKED_ACHIEVEMENT_RUNTIME_CACHE[id] then
+        return
+    end
+    UNLOCKED_ACHIEVEMENT_RUNTIME_CACHE[id] = true
     local luasteam = Steam.getSteam()
     if luasteam then
         luasteam.setAchievement(id)
