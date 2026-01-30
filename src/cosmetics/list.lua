d66388820cc39fe2aa115e040b33af982a14d188
Oli <OliverWDGarrett@gmail.com>
2026-01-30 14:04:12 +1300

    oops master broke

--- src/cosmetics/list.lua
+++ src/cosmetics/list.lua
@@ -1,4 +1,4 @@
--- Auto-generated at 2026-01-29 03:51:18.091394+00:00.
+-- Auto-generated at 2026-01-30 01:03:56.774137+00:00.
 -- DO NOT EDIT! Changes on this file will be lost!
 -- Modify tooling/make_cosmetics.py then re-run the script!
 
@@ -68,115 +68,113 @@ return function(defineCosmetic)
     map['yellow'] = 1031 map[1031] = 'yellow'
     defineCosmetic('BACKGROUND', 'zebra', 'Zebra', {})
     map['zebra'] = 1032 map[1032] = 'zebra'
-    defineCosmetic('AVATAR', 'actuallyinvisiblecat', 'Actually Invisible Cat', {})
-    map['actuallyinvisiblecat'] = 1033 map[1033] = 'actuallyinvisiblecat'
     defineCosmetic('AVATAR', 'angelcat', 'Angel Cat', {})
-    map['angelcat'] = 1034 map[1034] = 'angelcat'
+    map['angelcat'] = 1033 map[1033] = 'angelcat'
     defineCosmetic('AVATAR', 'angrycat', 'Angry Cat', {})
-    map['angrycat'] = 1035 map[1035] = 'angrycat'
+    map['angrycat'] = 1034 map[1034] = 'angrycat'
     defineCosmetic('AVATAR', 'blackcat', 'Black Cat', {})
-    map['blackcat'] = 1036 map[1036] = 'blackcat'
+    map['blackcat'] = 1035 map[1035] = 'blackcat'
     defineCosmetic('AVATAR', 'blankcat', 'Blank Cat', {})
-    map['blankcat'] = 1037 map[1037] = 'blankcat'
+    map['blankcat'] = 1036 map[1036] = 'blankcat'
     defineCosmetic('AVATAR', 'brownsuitcat', 'Brown Suit Cat', {})
-    map['brownsuitcat'] = 1038 map[1038] = 'brownsuitcat'
+    map['brownsuitcat'] = 1037 map[1037] = 'brownsuitcat'
     defineCosmetic('AVATAR', 'catptain', 'Catptain', {})
-    map['catptain'] = 1039 map[1039] = 'catptain'
+    map['catptain'] = 1038 map[1038] = 'catptain'
     defineCosmetic('AVATAR', 'cutecat', 'Cute Cat', {})
-    map['cutecat'] = 1040 map[1040] = 'cutecat'
+    map['cutecat'] = 1039 map[1039] = 'cutecat'
     defineCosmetic('AVATAR', 'cyclopscat', 'Cyclops Cat', {})
-    map['cyclopscat'] = 1041 map[1041] = 'cyclopscat'
+    map['cyclopscat'] = 1040 map[1040] = 'cyclopscat'
     defineCosmetic('AVATAR', 'demonicat', 'Demonicat', {})
-    map['demonicat'] = 1042 map[1042] = 'demonicat'
+    map['demonicat'] = 1041 map[1041] = 'demonicat'
     defineCosmetic('AVATAR', 'diamondcat', 'Diamond Cat', {})
-    map['diamondcat'] = 1043 map[1043] = 'diamondcat'
+    map['diamondcat'] = 1042 map[1042] = 'diamondcat'
     defineCosmetic('AVATAR', 'emeraldcat', 'Emerald cat', {})
-    map['emeraldcat'] = 1044 map[1044] = 'emeraldcat'
+    map['emeraldcat'] = 1043 map[1043] = 'emeraldcat'
     defineCosmetic('AVATAR', 'fatcat', 'Fat Cat', {})
-    map['fatcat'] = 1045 map[1045] = 'fatcat'
+    map['fatcat'] = 1044 map[1044] = 'fatcat'
     defineCosmetic('AVATAR', 'goldencat', 'Golden Cat', {})
-    map['goldencat'] = 1046 map[1046] = 'goldencat'
+    map['goldencat'] = 1045 map[1045] = 'goldencat'
     defineCosmetic('AVATAR', 'graycat', 'Gray Cat', {})
-    map['graycat'] = 1047 map[1047] = 'graycat'
+    map['graycat'] = 1046 map[1046] = 'graycat'
     defineCosmetic('AVATAR', 'greenscarfcat', 'Green Scarf Cat', {})
-    map['greenscarfcat'] = 1048 map[1048] = 'greenscarfcat'
+    map['greenscarfcat'] = 1047 map[1047] = 'greenscarfcat'
     defineCosmetic('AVATAR', 'invisiblecat', 'Invisible Cat', {})
-    map['invisiblecat'] = 1049 map[1049] = 'invisiblecat'
+    map['invisiblecat'] = 1048 map[1048] = 'invisiblecat'
     defineCosmetic('AVATAR', 'negacat', 'Negative Cat', {})
-    map['negacat'] = 1050 map[1050] = 'negacat'
+    map['negacat'] = 1049 map[1049] = 'negacat'
     defineCosmetic('AVATAR', 'orangecat', 'Orange Cat', {})
-    map['orangecat'] = 1051 map[1051] = 'orangecat'
+    map['orangecat'] = 1050 map[1050] = 'orangecat'
     defineCosmetic('AVATAR', 'plushcat', 'Plush Cat', {})
-    map['plushcat'] = 1052 map[1052] = 'plushcat'
+    map['plushcat'] = 1051 map[1051] = 'plushcat'
     defineCosmetic('AVATAR', 'purplescarfcat', 'Purple Scarf Cat', {})
-    map['purplescarfcat'] = 1053 map[1053] = 'purplescarfcat'
+    map['purplescarfcat'] = 1052 map[1052] = 'purplescarfcat'
     defineCosmetic('AVATAR', 'quizzicalcat', 'Quizzical Cat', {})
-    map['quizzicalcat'] = 1054 map[1054] = 'quizzicalcat'
+    map['quizzicalcat'] = 1053 map[1053] = 'quizzicalcat'
     defineCosmetic('AVATAR', 'reanimatedcat', 'Re-Animated Cat', {})
-    map['reanimatedcat'] = 1055 map[1055] = 'reanimatedcat'
+    map['reanimatedcat'] = 1054 map[1054] = 'reanimatedcat'
     defineCosmetic('AVATAR', 'redscarfcat', 'Red Scarf Cat', {})
-    map['redscarfcat'] = 1056 map[1056] = 'redscarfcat'
+    map['redscarfcat'] = 1055 map[1055] = 'redscarfcat'
     defineCosmetic('AVATAR', 'rubycat', 'Ruby Cat', {})
-    map['rubycat'] = 1057 map[1057] = 'rubycat'
+    map['rubycat'] = 1056 map[1056] = 'rubycat'
     defineCosmetic('AVATAR', 'sadcat', 'Sad Cat', {})
-    map['sadcat'] = 1058 map[1058] = 'sadcat'
+    map['sadcat'] = 1057 map[1057] = 'sadcat'
     defineCosmetic('AVATAR', 'silvercat', 'Silver Cat', {})
-    map['silvercat'] = 1059 map[1059] = 'silvercat'
+    map['silvercat'] = 1058 map[1058] = 'silvercat'
     defineCosmetic('AVATAR', 'stripedcat', 'Striped Cat', {})
-    map['stripedcat'] = 1060 map[1060] = 'stripedcat'
+    map['stripedcat'] = 1059 map[1059] = 'stripedcat'
     defineCosmetic('AVATAR', 'surprisedcat', 'Surprised Cat', {})
-    map['surprisedcat'] = 1061 map[1061] = 'surprisedcat'
+    map['surprisedcat'] = 1060 map[1060] = 'surprisedcat'
     defineCosmetic('AVATAR', 'triplecat', 'Triple Cat', {})
-    map['triplecat'] = 1062 map[1062] = 'triplecat'
+    map['triplecat'] = 1061 map[1061] = 'triplecat'
     defineCosmetic('AVATAR', 'tuffcat', 'Tuff Cat', {})
-    map['tuffcat'] = 1063 map[1063] = 'tuffcat'
+    map['tuffcat'] = 1062 map[1062] = 'tuffcat'
     defineCosmetic('AVATAR', 'tuxcat', 'Tuxedo Cat', {})
-    map['tuxcat'] = 1064 map[1064] = 'tuxcat'
+    map['tuxcat'] = 1063 map[1063] = 'tuxcat'
     defineCosmetic('HAT', 'blackcap', 'Black Cap', {offsetX = -1.0, offsetY = 8.0})
-    map['blackcap'] = 1065 map[1065] = 'blackcap'
+    map['blackcap'] = 1064 map[1064] = 'blackcap'
     defineCosmetic('HAT', 'bluecap', 'Blue Cap', {offsetX = 1.5, offsetY = 5.0})
-    map['bluecap'] = 1066 map[1066] = 'bluecap'
+    map['bluecap'] = 1065 map[1065] = 'bluecap'
     defineCosmetic('HAT', 'buckethat', 'Bucket Hat', {offsetY = 11.0})
-    map['buckethat'] = 1067 map[1067] = 'buckethat'
+    map['buckethat'] = 1066 map[1066] = 'buckethat'
     defineCosmetic('HAT', 'conehat', 'Cone Hat', {offsetY = 6.0})
-    map['conehat'] = 1068 map[1068] = 'conehat'
+    map['conehat'] = 1067 map[1067] = 'conehat'
     defineCosmetic('HAT', 'cowboyhat', 'Cowboy Hat', {offsetY = 6.0})
-    map['cowboyhat'] = 1069 map[1069] = 'cowboyhat'
+    map['cowboyhat'] = 1068 map[1068] = 'cowboyhat'
     defineCosmetic('HAT', 'crown', 'Crown', {offsetY = 5.0})
-    map['crown'] = 1070 map[1070] = 'crown'
+    map['crown'] = 1069 map[1069] = 'crown'
     defineCosmetic('HAT', 'divinghelmet', 'Diving Helmet', {offsetY = 12.0})
-    map['divinghelmet'] = 1071 map[1071] = 'divinghelmet'
+    map['divinghelmet'] = 1070 map[1070] = 'divinghelmet'
     defineCosmetic('HAT', 'fishinghat', 'Fishing Hat', {offsetX = -0.5, offsetY = 6.0})
-    map['fishinghat'] = 1072 map[1072] = 'fishinghat'
+    map['fishinghat'] = 1071 map[1071] = 'fishinghat'
     defineCosmetic('HAT', 'ghost', 'Ghost', {offsetX = -0.5, offsetY = 15.0})
-    map['ghost'] = 1073 map[1073] = 'ghost'
+    map['ghost'] = 1072 map[1072] = 'ghost'
     defineCosmetic('HAT', 'halo', 'Halo', {offsetY = 2.0})
-    map['halo'] = 1074 map[1074] = 'halo'
+    map['halo'] = 1073 map[1073] = 'halo'
     defineCosmetic('HAT', 'kitten', 'Kitten', {offsetY = 4.0})
-    map['kitten'] = 1075 map[1075] = 'kitten'
+    map['kitten'] = 1074 map[1074] = 'kitten'
     defineCosmetic('HAT', 'mafiahat', 'Mafia Hat', {offsetY = 5.0})
-    map['mafiahat'] = 1076 map[1076] = 'mafiahat'
+    map['mafiahat'] = 1075 map[1075] = 'mafiahat'
     defineCosmetic('HAT', 'monkblindfold', 'Monk Blindfold', {offsetX = -0.5, offsetY = 8.0})
-    map['monkblindfold'] = 1077 map[1077] = 'monkblindfold'
+    map['monkblindfold'] = 1076 map[1076] = 'monkblindfold'
     defineCosmetic('HAT', 'orangecap', 'Orange Cap', {offsetX = 1.5, offsetY = 5.0})
-    map['orangecap'] = 1078 map[1078] = 'orangecap'
+    map['orangecap'] = 1077 map[1077] = 'orangecap'
     defineCosmetic('HAT', 'pinkcap', 'Pink Cap', {offsetX = 0.5, offsetY = 9.0})
-    map['pinkcap'] = 1079 map[1079] = 'pinkcap'
+    map['pinkcap'] = 1078 map[1078] = 'pinkcap'
     defineCosmetic('HAT', 'piratehat', 'Pirate Hat', {offsetX = -0.5, offsetY = 6.0})
-    map['piratehat'] = 1080 map[1080] = 'piratehat'
+    map['piratehat'] = 1079 map[1079] = 'piratehat'
     defineCosmetic('HAT', 'rainbowcap', 'Rainbow Cap', {offsetX = 1.5, offsetY = 5.0})
-    map['rainbowcap'] = 1081 map[1081] = 'rainbowcap'
+    map['rainbowcap'] = 1080 map[1080] = 'rainbowcap'
     defineCosmetic('HAT', 'redcap', 'Red Cap', {offsetX = 1.5, offsetY = 5.0})
-    map['redcap'] = 1082 map[1082] = 'redcap'
+    map['redcap'] = 1081 map[1081] = 'redcap'
     defineCosmetic('HAT', 'stache', 'Stache', {offsetX = 0.5, offsetY = 13.0})
-    map['stache'] = 1083 map[1083] = 'stache'
+    map['stache'] = 1082 map[1082] = 'stache'
     defineCosmetic('HAT', 'tinyhat', 'Tiny Hat', {offsetY = 4.0})
-    map['tinyhat'] = 1084 map[1084] = 'tinyhat'
+    map['tinyhat'] = 1083 map[1083] = 'tinyhat'
     defineCosmetic('HAT', 'tophat', 'Top Hat', {offsetY = 5.0})
-    map['tophat'] = 1085 map[1085] = 'tophat'
+    map['tophat'] = 1084 map[1084] = 'tophat'
     defineCosmetic('HAT', 'whitecap', 'White Cap', {offsetX = 1.5, offsetY = 5.0})
-    map['whitecap'] = 1086 map[1086] = 'whitecap'
+    map['whitecap'] = 1085 map[1085] = 'whitecap'
     defineCosmetic('HAT', 'yellowcap', 'Yellow Cap', {offsetX = 1.5, offsetY = 5.0})
-    map['yellowcap'] = 1087 map[1087] = 'yellowcap'
+    map['yellowcap'] = 1086 map[1086] = 'yellowcap'
     return map
 end
