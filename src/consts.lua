f6c77d18dfac482acd4b548b860463928cc7c446
Miku AuahDark <lumi.hotaru@gmail.com>
2026-02-02 12:10:21 +0800

    Initial cosmetic wire with Steam inventory.

--- src/consts.lua
+++ src/consts.lua
@@ -15,6 +15,7 @@ local consts = {
     -- How long it should take before sending "update" event to analytics server (in seconds)?
     ANALYTICS_UPDATE_INTERVAL = 60,
     GAME_VERSION = 0,
+    ANALYTICS_IDENTITY = "incremental_game",
 
     FILE_LOG_LEVEL = "warn",
     CONSOLE_LOG_LEVEL = "trace",
@@ -57,6 +58,7 @@ local consts = {
     DEFAULT_CAT_AVATAR = "cat",
     DEFAULT_BACKGROUND_AVATAR = "white",
     DEFAULT_SCYTHE = "starting_scythe",
+    STEAM_CHEST_ITEMDEFID = 1,
 
     ORBIT_RING_DISTANCE = 20, -- Radius of each orbit ring.
 
