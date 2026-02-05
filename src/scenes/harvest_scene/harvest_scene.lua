6d215e6c5867da022445d7d6259623fa7cbf092f
Miku AuahDark <lumi.hotaru@gmail.com>
2026-02-05 15:19:10 +0800

    Close prestige popup after pressing "Prestige".

--- src/scenes/harvest_scene/harvest_scene.lua
+++ src/scenes/harvest_scene/harvest_scene.lua
@@ -670,6 +670,7 @@ function drawBossPopup(self)
     if iml.wasJustClicked(buttonArea:get()) then
         g.playUISound("ui_click_basic", 1.4, 0.8)
         g.incrementPrestige()
+        self.bossPopup = false
     end
 
     prof_pop()
