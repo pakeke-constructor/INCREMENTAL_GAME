a92c907c5feb074c84c64629acd0e7aa7b5443e5
Miku AuahDark <lumi.hotaru@gmail.com>
2026-02-10 09:11:14 +0800

    Generalize boss preview drawing function.

--- src/scenes/boss_scene/boss_scene.lua
+++ src/scenes/boss_scene/boss_scene.lua
@@ -99,21 +99,18 @@ local function drawBossButtonStuff(self, bob)
 end
 
 ---@param r kirigami.Region
-local function drawPumpkinBoss(r)
+---@param tokInfo g.TokenInfo
+local function drawBoss(r, tokInfo)
     local px, py = r:getCenter()
-    g.drawImage("pumpkin_boss", px, py, 0, 2, 2)
-end
+    g.drawImage(tokInfo.image, px, py, 0, 2, 2)
 
----@param r kirigami.Region
-local function drawCrabBoss(r)
-    local px, py = r:getCenter()
-    local t = love.timer.getTime() / 3
-    local rotleft = math.sin(t * 2 * math.pi) * 0.4
-    local rotright = math.cos(t * 2 * math.pi) * 0.4
-
-    g.drawImage("giantcrab_body", px, py, 0, 2, 2)
-    g.drawImageOffset("giantcrab_claw", px + 64, py + 38, rotright, 2, 2, 0.6, 0.2)
-    g.drawImageOffset("giantcrab_claw", px - 64, py + 38, rotleft, -2, 2, 0.6, 0.2)
+    if tokInfo.type == "giantcrab_boss" then
+        local t = love.timer.getTime() / 3
+        local rotleft = math.sin(t * 2 * math.pi) * 0.4
+        local rotright = math.cos(t * 2 * math.pi) * 0.4
+        g.drawImageOffset("giantcrab_claw", px + 64, py + 38, rotright, 2, 2, 0.6, 0.2)
+        g.drawImageOffset("giantcrab_claw", px - 64, py + 38, rotleft, -2, 2, 0.6, 0.2)
+    end
 end
 
 function boss:draw()
@@ -156,12 +153,8 @@ function boss:draw()
         :padRatio(0.6,0,0.6,0)
         :moveUnit(0,bob)
     local pumpkin,a,b,_ = r2:splitVertical(3,1,1,2)
-    local prestige = g.getSn().prestige
-    if prestige == 0 then
-        drawPumpkinBoss(pumpkin)
-    elseif prestige == 1 then
-        drawCrabBoss(pumpkin)
-    end
+    local bossId = assert(g.getBossIdForPrestige(g.getPrestige()))
+    drawBoss(pumpkin, g.getTokenInfo(bossId))
     richtext.printRichContained(SUMMON_BOSS, f, a:get())
     richtext.printRichContained(BOSS_INFO, f, b:get())
     end
