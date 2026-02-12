fc956fee0da874064aab239d2eb44aae94ae41a6
Oli <OliverWDGarrett@gmail.com>
2026-02-12 20:10:54 +1300

    adjusted stats

--- src/g.lua
+++ src/g.lua
@@ -661,8 +661,8 @@ g.stats = {}
 
 -- SSTATS 
 -- (if you ever want to quickly search the name of stats, search "sstats")
-g.stats.HitSpeed = g.defineStat("HitSpeed", 5, "Hit Speed")
-g.stats.HitDamage = g.defineStat("HitDamage", 20, "Hit Damage")
+g.stats.HitSpeed = g.defineStat("HitSpeed", 9, "Hit Speed")
+g.stats.HitDamage = g.defineStat("HitDamage", 25, "Hit Damage")
 g.stats.HarvestArea = g.defineStat("HarvestArea", 10, "Harvest Area")
 g.stats.ResourceMultiplier = g.defineStat("ResourceMultiplier", 1, "Resource Gain Multiplier")
 g.stats.OrbitSpeed = g.defineStat("OrbitSpeed", 2, "Entity Orbit Speed") -- rad/s
